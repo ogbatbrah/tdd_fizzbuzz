d12db76313ab506f70f763065326aa633153624b
jacob <jakechap08@gmail.com>
2018-06-20 15:54:07 -0400

    failed first test

--- test_tdd_fizzbuzz.rb
+++ test_tdd_fizzbuzz.rb
@@ -4,7 +4,10 @@ require_relative "tdd_fizzbuzz.rb"
 class TestTddFizzBuzz < Minitest::Test
 
     def test_assert_that_1_equals_1
-        assert_equal(1, 1)
+    	assert_equal(1, 1)
     end
-
+    
+    def test_assert_that_end_result_is_array
+    	assert_equal(Array, fizzbuzz().class)
+    end
 end
