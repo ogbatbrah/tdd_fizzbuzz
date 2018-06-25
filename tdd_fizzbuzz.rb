66d425e22ca9e32f71ffc5a4e9e1212925243b50
jacob <jakechap08@gmail.com>
2018-06-25 10:40:58 -0400

    finshed product from class wed.

--- tdd_fizzbuzz.rb
+++ tdd_fizzbuzz.rb
@@ -1,7 +1,18 @@
 def fizzbuzz()
-	fizzarr = [*1..100]
-	fizzarr[2] = "mined"
+    fizzarr = [*1..100]
+    fizzarr[2] = "mined"
     fizzarr[4] = "minds"
     fizzarr[14] = "mined minds"
     fizzarr
 end
+    # fizzarr.each do |item| 
+    #     if item 
+    #         item = "mined minds"
+    #     elsif item 
+    #         item = "mined"
+    #     elsif item\
+    #         item = "minds"
+    #     end
+    #     new_arr << item
+    # end
+    # new_arr
--- test_tdd_fizzbuzz.rb
+++ test_tdd_fizzbuzz.rb
@@ -1,33 +1,34 @@
+
 require "minitest/autorun"
 require_relative "tdd_fizzbuzz.rb"
 
 class TestTddFizzBuzz < Minitest::Test
 
     def test_assert_that_1_equals_1
-    	assert_equal(1, 1)
+        assert_equal(1, 1)
     end
     
     def test_assert_that_end_result_is_array
-    	assert_equal(Array, fizzbuzz().class)
+        assert_equal(Array, fizzbuzz().class)
     end
 
     def test_assert_array_length_is_100
-    	assert_equal(100, fizzbuzz().length)
+        assert_equal(100, fizzbuzz().length)
     end
 
     def test_assert_first_position_is_1
-    	assert_equal(1, fizzbuzz().first)
+        assert_equal(1, fizzbuzz().first)
     end
     
     def test_assert_third_positiion_is_mined
-    	assert_equal("mined", fizzbuzz()[2])
+        assert_equal("mined", fizzbuzz()[2])
     end
 
     def test_asert_fifth_position_is_minds
-    	assert_equal("minds", fizzbuzz()[4])
+        assert_equal("minds", fizzbuzz()[4])
     end
+   
     def test_assert_fifteenth_position_is_mined_minds
-    	assert_equal("mined minds", fizzbuzz()[14])
+        assert_equal("mined minds", fizzbuzz()[14])
     end
 end
-
